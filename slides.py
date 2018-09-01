b5c005fcbbd938cc25661fa26fca6664f63270e8
krzysztofarendt <krzysztof.arendt@gmail.com>
2018-09-01 15:34:20 +0200

    Update Lecture 1

---
+++ slides.py
@@ -0,0 +1,23 @@
+"""
+This scripts generates a slideshow for a specified notebook.
+
+Usage:
+python slides.py Lecture_X
+
+Lecture_X is the name of the notebook to be converted.
+"""
+
+import os
+import sys
+from subprocess import call
+
+if len(sys.argv) > 1:
+    lecture = sys.argv[1]
+else:
+    raise RuntimeError("Lecture name not provided!")
+
+call([
+    "jupyter", "nbconvert",
+    os.getcwd() + "\\{}.ipynb".format(lecture),
+    "--to", "slides",
+    "--post", "serve"])
