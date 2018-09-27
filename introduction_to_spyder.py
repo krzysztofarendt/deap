482edc001c01f654e65d1c6f59f53ab33d95247a
krzysztofarendt <krzysztof.arendt@gmail.com>
2018-09-27 16:26:46 +0200

    Add Lecture 3. Add some slides to Lecture 2

---
+++ introduction_to_spyder.py
@@ -0,0 +1,31 @@
+# To be presented:
+# ============================
+# 1. Working directory
+# 2. Code sections
+# 3. Interactive interpreter
+# 4. Help / File explorer / Variable explorer
+# 5. Write simple program and execute
+
+#%%
+age = input('What is your age? ')
+age = float(age)
+
+#%%
+if age > 30:
+    print("You're old, ha ha")
+else:
+    print("You will be old soon, ha ha")
+
+#%%
+to_do = ['Wake up', 'Shower', 'Eat', 'Go to school']
+
+#%%
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+#%%
+def func():
+    print('Hello world!')
+    
+#%%
+for x in range(5):
+    func()
